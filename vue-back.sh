0bc3ac32f5b7f4035486e40ef945f895f28371a1
meijh <meijh@fosun.com>
2023-05-11 02:35:34 +0800

    test

--- vue-back.sh
+++ vue-back.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-WORK_PATH='/root/projects/docker-nginx-cicd/vue-back'
+WORK_PATH='/root/jhmei/projects/vue-back'
 cd $WORK_PATH
 echo "先清除老代码"
-git reset --hard origin/master
+git reset --hard origin/main
 git clean -f
 echo "拉取最新代码"
-git pull origin master
+git pull origin main
 echo "开始执行构建"
 docker build -t vue-back:1.0 .
 echo "停止旧容器并删除旧容器"
